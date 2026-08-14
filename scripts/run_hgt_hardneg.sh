165069ce7d016225498e48ebe16c9c153ba5697a
Xingyuan Zhang <senecio@stanford.edu>
2026-08-13 19:16:42 -0700

    Updating HGT params for Marlowe cluster

--- scripts/run_hgt_hardneg.sh
+++ scripts/run_hgt_hardneg.sh
@@ -5,7 +5,7 @@ python scripts/run_graphsage.py \
   --graph-type hetero \
   --encoder hgt \
   --hgt-heads 4 \
-  --hgt-dropout 0.2 \
+  --hgt-dropout 0.20 \
   --hgt-layers 2 \
   --decoder mlp \
   --negative-mode two_hop_hard \
--- scripts/run_hgt_node_disjoint.sh
+++ scripts/run_hgt_node_disjoint.sh
@@ -5,7 +5,7 @@ python scripts/run_graphsage.py \
   --graph-type hetero \
   --encoder hgt \
   --hgt-heads 4 \
-  --hgt-dropout 0.2 \
+  --hgt-dropout 0.20 \
   --hgt-layers 2 \
   --decoder mlp \
   --split-mode node_disjoint \
